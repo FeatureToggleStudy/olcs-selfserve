2e9d0504fb046cd975bd11aae9bf0b13bb683cc3
Jason de Jonge <dejongej@Jasons-MacBook-Pro.local>
2018-07-11 14:04:54 +0100

    made the list of licences dynamic to use the list returned by backend

--- test/Permits/src/Form/Model/Form/LicenceSelectionFormTest.php
+++ test/Permits/src/Form/Model/Form/LicenceSelectionFormTest.php
@@ -14,33 +14,22 @@ class LicenceSelectionFormTest extends AbstractFormValidationTestCase
     /**
      * @var string The class name of the form being tested
      */
-    protected $formName = \Permits\Form\Model\Form\LicenceSelectionForm::class;
+    protected $formName = \Permits\Form\Model\Form\EcmtLicenceForm::class;
 
-    /*public function testLicence()
+    public function testLicence()
     {
-        $element = ['Fields', 'Licence'];
+        $element = ['Fields', 'EcmtLicence'];
 
         $this->assertFormElementRequired($element, true);
         $this->assertFormElementAllowEmpty($element, false);
         $this->assertFormElementType($element, "Zend\Form\Element\Radio");
     }
 
-    public function testGuidance()
-    {
-        $element = ['Fields', 'Guidance'];
-
-        $this->assertFormElementRequired($element, false);
-        $this->assertFormElementAllowEmpty($element, true);
-        $this->assertFormElementType($element, "\Common\Form\Elements\Types\GuidanceTranslated");
-        $this->assertAttributeEquals("guidance", "data-container-class", $element);
-    }
-
-
     public function testSubmit()
     {
         $element = ['Submit', 'SubmitButton'];
         $this->assertFormElementActionButton($element);
         $this->assertFormElementType($element, "Zend\Form\Element\Submit");
-    }*/
+    }
 
 }
