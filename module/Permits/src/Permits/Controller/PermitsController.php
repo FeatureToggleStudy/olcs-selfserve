17ee187284651780c7645dd06da84a96d23694d6
Kollol Shamsuddin <kol.shamsuddin@capgemini.com>
2018-07-26 10:44:52 +0100

    Sorting out the custom validation messages for restricted countries

--- module/Permits/src/Permits/Controller/PermitsController.php
+++ module/Permits/src/Permits/Controller/PermitsController.php
@@ -261,7 +261,7 @@ class PermitsController extends AbstractOlcsController implements ToggleAwareInt
                 }
             }else{
                 //Custom Error Message
-                $form->get('Fields')->get('restrictedCountriesList')->setMessages(['error.messages.restricted.countries']);
+                $form->get('Fields')->get('restrictedCountries')->setMessages(['error.messages.restricted.countries']);
             }
         }
 
