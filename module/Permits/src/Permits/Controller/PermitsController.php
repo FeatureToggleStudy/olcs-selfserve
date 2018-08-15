63870c6aff2c096350e74f54343c753931aedb42
Jason de Jonge <dejongej@Jasons-MacBook-Pro.local>
2018-08-15 16:38:08 +0100

    merge conflict

--- module/Permits/src/Permits/Controller/PermitsController.php
+++ module/Permits/src/Permits/Controller/PermitsController.php
@@ -276,13 +276,11 @@ class PermitsController extends AbstractOlcsController implements ToggleAwareInt
         /*
         * Get Countries List from Database
         */
-        $response = $this->handleQuery(ConstrainedCountries::create([]));
-        $restrictedCountryList = $response->getResult();
 
         /*
         * Make the restricted countries list the value_options of the form
         */
-        $restrictedCountryList = $this->getServiceLocator()
+       /* $restrictedCountryList = $this->getServiceLocator()
             ->get('Helper\Form')
             ->transformListIntoValueOptions($restrictedCountryList, 'description');
 
@@ -291,12 +289,12 @@ class PermitsController extends AbstractOlcsController implements ToggleAwareInt
         $form->get('Fields')
             ->get('restrictedCountriesList')
             ->get('restrictedCountriesList')
-            ->setOptions($options);
+            ->setOptions($options);*/
 
         // Read data
         $application = $this->getApplication($id);
 
-        if (count($application['countrys']) > 0) {
+       /* if (count($application['countrys']) > 0) {
             $form->get('Fields')
                 ->get('restrictedCountries')
                 ->setValue('1');
@@ -312,7 +310,7 @@ class PermitsController extends AbstractOlcsController implements ToggleAwareInt
                 ->get('restrictedCountriesList')
                 ->get('restrictedCountriesList')
                 ->setValue($selectedValues);
-        }
+        }*/
 
         $data = $this->params()->fromPost();
 
